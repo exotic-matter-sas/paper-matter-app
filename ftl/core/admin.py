3101f34f8428483c88b9b4a27b6efd51af3ef97e
Antoine L <antoine@exotic-matter.fr>
2019-02-23 23:46:48 +0100

    Added django-mptt for handling of folders Adjusted models (FTLDocument, FTLFolder) for django-mptt Added __str__ for all models for better visibility in admin Added admin for folders
    ref #3

--- ftl/core/admin.py
+++ ftl/core/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-
 # Register your models here.
-from core.models import FTLOrg, FTLUser, FTLDocument
+from mptt.admin import MPTTModelAdmin
+
+from core.models import FTLOrg, FTLUser, FTLDocument, FTLFolder
 
 admin.site.register(FTLOrg)
 admin.site.register(FTLUser)
 admin.site.register(FTLDocument)
+admin.site.register(FTLFolder, MPTTModelAdmin)
--- ftl/core/models.py
+++ ftl/core/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from mptt.fields import TreeForeignKey
+from mptt.models import MPTTModel
 
 
 # FTP orgs
@@ -8,6 +10,9 @@ class FTLOrg(models.Model):
     slug = models.SlugField(max_length=128)  # URL of the org
     created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
 
 # FTL user (linked to Django user)
 class FTLUser(models.Model):
@@ -15,12 +20,31 @@ class FTLUser(models.Model):
     org = models.ForeignKey('FTLOrg', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.ftl_user.__str__()
+
 
 # FTL Documents
 class FTLDocument(models.Model):
     org = models.ForeignKey('FTLOrg', on_delete=models.CASCADE)
     ftl_user = models.ForeignKey('FTLUser', on_delete=models.CASCADE)
+    ftl_folder = TreeForeignKey('FTLFolder', on_delete=models.CASCADE, null=True, blank=True)
     title = models.TextField()
     note = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     edited = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+# FTL Folders
+class FTLFolder(MPTTModel):
+    name = models.CharField(max_length=128)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    def __str__(self):
+        return self.name
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
