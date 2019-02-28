17eae6f3e5c0ee34566ade685622b923b0d64f94
Fabien Daveine <fabien@exotic-matter.fr>
2019-02-28 11:06:23 +0100

    Simplify setup.tests.py

--- ftl/setup/tests.py
+++ ftl/setup/tests.py
@@ -1,19 +1,9 @@
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
-
-from .views import LandingPageView
 
 
 class SetupPageTest(TestCase):
 
-    def test_setup_url_resolve_landing_page_view(self):
-        found = resolve('/setup/')
-        self.assertEqual(found.func, LandingPageView.as_view())
-
     def test_landing_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = LandingPageView.as_view(request)
-        self.assertTrue(response.content.startswith(b'<!DOCTYPE html>'))
-        self.assertIn(b'Landing page', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        response = self.client.get('/setup/')
+        self.assertContains(response, 'Landing page')
+        self.assertTemplateUsed(response, 'setup/admin_and_first_organization_creation_form.html')
