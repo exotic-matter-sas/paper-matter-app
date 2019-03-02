41c7f9c3570114c80cb102c4887adbf99fa24060
Antoine L <antoine@exotic-matter.fr>
2019-03-02 01:27:22 +0100

    Use count() for checking presence of admin and org. Refactored logic by case to happen most of the time

--- ftl/ftl/views.py
+++ ftl/ftl/views.py
@@ -1,16 +1,15 @@
-from django.shortcuts import redirect
 from django.contrib.auth.models import User
+from django.shortcuts import redirect
+
 from core.models import FTLOrg
 
 
 def index(request):
-    admin_users = User.objects.filter(is_staff=True)
-    if not admin_users:
-        return redirect('setup:landing_page_step1')
-    else:
-        organizations = FTLOrg.objects.filter()
-        if not organizations:
-            return redirect('setup:landing_page_step2')
-        else:
+    admin_users = User.objects.filter(is_staff=True).count()
+    if admin_users:
+        if FTLOrg.objects.count():
             return redirect('app:login')
-
+        else:
+            return redirect('setup:landing_page_step2')
+    else:
+        return redirect('setup:landing_page_step1')
