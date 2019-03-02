0f92d989aebe2baf4fdf878cb7adf2bb04be083b
Antoine L <antoine@exotic-matter.fr>
2019-03-02 20:22:45 +0100

    Login view with Django standard LoginView class

--- ftl/core/urls.py
+++ ftl/core/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+
 from . import views
 
 app_name = 'app'
 urlpatterns = [
     path('login/', views.login_hub, name='login_hub'),
-    path('<str:org_slug>/login/', views.login, name='login'),
-    path('<str:org_slug>/signup/', views.signup, name='signup'),
-    path('<str:org_slug>/signup/success', views.signup_success, name='signup_success'),
-]
+    # path('<str:org_slug>/login/', views.login, name='login'),
+    # path('<str:org_slug>/signup/', views.signup, name='signup'),
+    # path('<str:org_slug>/signup/success', views.signup_success, name='signup_success'),
 
+    path('<slug:org_slug>/', include('django.contrib.auth.urls')),
+]
