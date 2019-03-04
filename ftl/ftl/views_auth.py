2ae81717a04ad70004908efa58f0f946f207a7b8
Antoine L <antoine@exotic-matter.fr>
2019-03-04 15:28:07 +0100

    Added comment about the custom auth views.

--- ftl/ftl/views_auth.py
+++ ftl/ftl/views_auth.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.views import PasswordResetView, PasswordChangeView, PasswordResetConfirmView
 from django.urls import reverse_lazy
 
+"""
+Classes based of Django standard auth view for handling of the `org_slug` parameter in URL.
+Overloading of get_success_url() to add url parameters to the view.
+"""
+
 
 class PasswordChangeViewFTL(PasswordChangeView):
     def get_success_url(self):
