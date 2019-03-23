934d94d517dfaa8b462d920517ab47180f203912
Fabien Daveine <fabien@exotic-matter.fr>
2019-03-23 23:15:17 +0100

    typo

--- ftl/setup/tests.py
+++ ftl/setup/tests.py
@@ -7,7 +7,7 @@ class SetupPagesTests(TestCase):
 
     def test_landing_page_step1_returns_correct_html(self):
         response = self.client.get('/setup/createorg/')
-        self.assertContains(response, 'Create first organization')
+        self.assertContains(response, 'create first organization')
         self.assertTemplateUsed(response, 'setup/first_organization_creation_form.html')
 
     def test_landing_page_step2_returns_correct_html(self):
