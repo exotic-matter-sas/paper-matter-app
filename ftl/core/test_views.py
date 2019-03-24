7199ddc5b1b037a6815331dd8ea02c55ce905e8f
Fabien Daveine <fabien@exotic-matter.fr>
2019-03-24 09:39:32 +0100

    Add core.test_views.test_home_get_proper_context test

--- ftl/core/test_views.py
+++ ftl/core/test_views.py
@@ -12,6 +12,14 @@ class CorePagesTests(TestCase):
         setup_authenticated_session(self.client, self.org, self.user)
 
     def test_home_page_returns_correct_html(self):
+        """Home page returns correct html"""
         response = self.client.get('/app/')
         self.assertContains(response, self.user.username)
         self.assertTemplateUsed(response, 'core/home.html')
+
+    def test_home_get_proper_context(self):
+        """Home page get proper context"""
+        response = self.client.get('/app/')
+        self.assertEqual(response.context['org_name'], self.org.name)
+        self.assertEqual(response.context['username'], self.user.username)
+
