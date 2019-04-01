f3daec7d217c6c56f4bdb8b467c38d3d379e8348
Antoine L <antoine@exotic-matter.fr>
2019-04-01 21:01:57 +0200

    Cleaning

--- ftl/core/serializers.py
+++ ftl/core/serializers.py
@@ -6,29 +6,5 @@ from core.models import FTLDocument
 class FTLDocumentSerializer(serializers.ModelSerializer):
     class Meta:
         model = FTLDocument
-        # fields = ('url', 'title', 'note', 'created', 'edited', 'ftl_user', 'ftl_folder', 'org')
-        fields = ('id', 'title', 'note', 'created', 'edited', 'ftl_folder')
+        fields = ('id', 'title', 'note', 'created', 'edited', 'ftl_folder',)
         read_only_fields = ('created', 'edited',)
-
-# class FTLOrgSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = FTLOrg
-#         fields = ('url', 'name', 'slug', 'created')
-#
-#
-# class FTLUserSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = FTLUser
-#         fields = ('url', 'created', 'org', 'user')
-
-
-# class UserSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ('url', 'username', 'email', 'groups')
-#
-#
-# class GroupSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = Group
-#         fields = ('url', 'name')
