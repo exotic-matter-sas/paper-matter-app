f8d286e73f73c0288515138d80e3ce91a632165f
Fabien Daveine <fabien@exotic-matter.fr>
2019-05-07 16:40:10 +0200

    Merge test_login_page_redirect_logged_user from master

--- ftl/ftests/login_tests.py
+++ ftl/ftests/login_tests.py
@@ -14,8 +14,8 @@ class LoginPageTests(LoginPage, HomePage):
 
     def test_first_user_can_login(self):
         """First user can login and access a logged page"""
-        # User login and is redirect to the logged home page
-        self.visit(self.url)
+        # User login and is redirected to the home page
+        self.visit(LoginPage.url)
         self.log_user()
 
         # He can see it's username on it
@@ -24,12 +24,24 @@ class LoginPageTests(LoginPage, HomePage):
 
     def test_login_failed(self):
         # User login and is redirect to the logged home page
-        self.visit(self.url)
+        self.visit(LoginPage.url)
         self.log_user(user_num=2)  # User2 doesn't exist
 
         # User stay on login page and an error message is displayed
         self.assertIn('login', self.head_title)
         self.assertIn('Please enter a correct username and password', self.get_elem(self.login_failed_div).text)
+
+    def test_login_page_redirect_logged_user(self):
+        """First user can login and access a logged page"""
+        # User login
+        self.visit(LoginPage.url)
+        self.log_user()
+
+        # User access login page again
+        self.visit(LoginPage.url)
+
+        # User is redirected to home page as he is already logged
+        self.assertIn('Home', self.browser.title)
 
     def test_password_forgotten(self):
         pass  # TODO
