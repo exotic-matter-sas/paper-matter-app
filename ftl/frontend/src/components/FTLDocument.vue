3fd299f4829053054eaac47264f5754eea59329b
Fabien Daveine <fabien@exotic-matter.fr>
2019-05-27 10:53:02 +0200

    Rename thumbnail url for clarity

--- ftl/frontend/src/components/FTLDocument.vue
+++ ftl/frontend/src/components/FTLDocument.vue
@@ -3,7 +3,7 @@
         <b-row class="text-truncate document-title"><span
             @click="$emit('event-open-doc', doc.pid)">{{ doc.title }}</span></b-row>
         <b-row align-h="center">
-            <b-img :src="'/app/api/v1/documents/' + doc.pid + '/th.png'" class="img-thumbnail" slot="aside"
+            <b-img :src="'/app/api/v1/documents/' + doc.pid + '/thumbnail.png'" class="img-thumbnail" slot="aside"
                    blank-color="#abc"
                    @click="$emit('event-open-doc', doc.pid)"
                    onerror="this.src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAJYAAADICAQAAACgjNDuAAABIElEQVR42u3QAQEAAAgCIP0/ui44ACbQXBhVlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVLlixZsmTJkiVr9/mzyAFGRN6/AAAAAElFTkSuQmCC\n'"/>
