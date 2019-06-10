c74ddf9b72d112cff548eb95ae0adc6a6a8b1fab
Antoine L <antoine@exotic-matter.fr>
2019-06-10 11:51:37 +0200

    Added interpolation support to i18n.

--- ftl/frontend/src/main.js
+++ ftl/frontend/src/main.js
@@ -11,11 +11,23 @@ Vue.config.productionTip = false;
 
 Vue.use(BootstrapVue);
 
-Vue.prototype.$_ = function (text) {
+Vue.prototype.$_ = function (text, vars = null) {
   var translated_text;
-  if (typeof gettext === 'function') {
-    translated_text = gettext(text);
+
+  if (vars === null) {
+    if (typeof gettext === 'function') {
+      translated_text = gettext(text);
+    }
+  } else {
+    if (typeof interpolate === 'function') {
+      if (Array.isArray(vars)) {
+        translated_text = interpolate(text, vars);
+      } else {
+        translated_text = interpolate(text, vars, true);
+      }
+    }
   }
+
   return translated_text || text;
 };
 
