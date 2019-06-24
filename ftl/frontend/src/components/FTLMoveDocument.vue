c681cee9d12d5141ae60dd5694d845a3af132d81
Antoine L <antoine@exotic-matter.fr>
2019-06-24 14:30:32 +0200

    WIP Move document feature

---
+++ ftl/frontend/src/components/FTLMoveDocument.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>export default {
+  name: 'FTLMoveDocument',
+  props: [],
+  data() {
+    return {}
+  },
+  methods: {}
+}
+
+</script>
+
+<style scoped>
+</style>
