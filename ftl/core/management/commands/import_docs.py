d0bef254d0d2a2dc977bb4842d46aa62f6b62364
Antoine L <antoine@exotic-matter.fr>
2019-07-01 10:48:11 +0200

    Added i18n to command import_docs

--- ftl/core/management/commands/import_docs.py
+++ ftl/core/management/commands/import_docs.py
@@ -1,10 +1,13 @@
 import os
 import pathlib
+import time
 from datetime import datetime
 
 from django.core.files.base import File
 from django.core.management import BaseCommand
 from django.utils import timezone
+from django.utils.translation import gettext as _
+from django.utils.translation import ngettext
 
 from core.models import FTLFolder, FTLDocument, FTLUser
 
@@ -18,29 +21,54 @@ class Command(BaseCommand):
         parser.add_argument('username', nargs='?', type=str)
 
     def handle(self, *args, **options):
-        self.user = FTLUser.objects.get(username=options['username'])
-        self._explore(options['path'])
+        self.stdout.write(
+            self.style.MIGRATE_HEADING(
+                _('Starting to migrate for %(username)s from %(path)s'
+                  % {'username': options['username'], 'path': options['path']}
+                  )
+            )
+        )
 
-    def _explore(self, path, ftl_parent_folder=None):
+        self.user = FTLUser.objects.get(username=options['username'])
+
+        start_time = time.time()
+        count = self._explore(options['path'])
+        end_time = time.time()
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                ngettext('One document successfully imported in %(time)s',
+                         '%(count)s documents successfully imported in %(time)s',
+                         count
+                         )
+                % {
+                    'count': count,
+                    'time': round(end_time - start_time, 2)
+                }
+            )
+        )
+
+    def _explore(self, path, ftl_parent_folder=None, count=0):
         current = os.scandir(path)
+        _count = count
 
         for directory in current:
             if directory.is_dir():
-                print("dir", directory.name)
-
                 folder = FTLFolder()
                 folder.name = directory.name
                 folder.parent = ftl_parent_folder
                 folder.org = self.user.org
                 folder.save()
 
-                self._explore(directory.path, folder)
+                self.stdout.write(self.style.SUCCESS(f'Created folder {directory.name}'))
+
+                _count += self._explore(directory.path, folder, count)
 
             elif directory.is_file():
                 file = directory
 
                 if pathlib.Path(file.path).suffix.lower() == '.pdf':
-                    print("file", file.name)
+                    self.stdout.write(self.style.SUCCESS(f'Imported document {file.name}'))
 
                     with open(file.path, 'rb') as f:
                         document = FTLDocument()
@@ -49,6 +77,10 @@ class Command(BaseCommand):
                         document.binary = File(f)
                         document.org = self.user.org
                         document.title = file.name
-                        document.created = timezone.make_aware(datetime.fromtimestamp(int(os.path.getmtime(file.path))),
-                                                               timezone.get_current_timezone())
+                        document.created = timezone.make_aware(
+                            datetime.fromtimestamp(int(os.path.getmtime(file.path))),
+                            timezone.get_current_timezone())
                         document.save()
+                        _count += 1
+
+        return _count
