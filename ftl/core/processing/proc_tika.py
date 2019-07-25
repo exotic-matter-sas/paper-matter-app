5588f74da10e86eb3a13aa639c2cc5ff6e9c4dc3
Antoine L <antoine@exotic-matter.fr>
2019-07-25 11:02:22 +0200

    Failsae when Tika is not working (report code)

--- ftl/core/processing/proc_tika.py
+++ ftl/core/processing/proc_tika.py
@@ -6,5 +6,6 @@ from core.processing.ftl_processing import FTLDocProcessingBase
 class FTLDocTextExtractionTika(FTLDocProcessingBase):
     def process(self, ftl_doc):
         parsed_txt = parser.from_buffer(ftl_doc.binary.read())
-        ftl_doc.content_text = parsed_txt["content"].strip()
-        ftl_doc.save()
+        if 'content' in parsed_txt and parsed_txt["content"]:
+            ftl_doc.content_text = parsed_txt["content"].strip()
+            ftl_doc.save()
