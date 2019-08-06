e812721186673b0b1fd524e50a21afef33451688
Fabien Daveine <fabien@exotic-matter.fr>
2019-08-06 08:12:50 +0200

    Add tests to test_processing

--- ftl/core/test_processing.py
+++ ftl/core/test_processing.py
@@ -1,13 +1,17 @@
-from unittest import TestCase, skip
+from concurrent.futures import wait as wait_futures
 from unittest.mock import Mock, patch
 
 import langid
+from django.test import TestCase
 from tika import parser
 
-from core.processing.ftl_processing import FTLDocumentProcessing, FTLDocProcessingBase
+from core.errors import PluginUnsupportedStorage
+from core.processing import ftl_processing
+from core.processing.ftl_processing import FTLDocumentProcessing, FTLDocProcessingBase, FTLOCRBase
 from core.processing.proc_lang import FTLLangDetectorLangId
 from core.processing.proc_pgsql_tsvector import FTLSearchEnginePgSQLTSVector, SEARCH_VECTOR
 from core.processing.proc_tika import FTLTextExtractionTika
+from ftl.settings import DEFAULT_FILE_STORAGE
 
 
 class ProcTest(FTLDocProcessingBase):
@@ -38,9 +42,9 @@ class DocumentProcessingTests(TestCase):
         self.processing.executor.submit.assert_called_once_with(self.processing._handle, doc)
 
     def test_handle(self):
-        mock_plugin_1 = Mock(spec=ProcTest)
-        mock_plugin_2 = Mock(spec=ProcTest)
-        mock_plugin_3 = Mock(spec=ProcTest)
+        mock_plugin_1 = Mock()
+        mock_plugin_2 = Mock()
+        mock_plugin_3 = Mock()
 
         self.processing.plugins = list()
         self.processing.plugins.append(mock_plugin_1)
@@ -48,15 +52,38 @@ class DocumentProcessingTests(TestCase):
         self.processing.plugins.append(mock_plugin_3)
 
         doc = Mock()
-        self.processing.apply_processing(doc)
+        future = self.processing.apply_processing(doc)
+        wait_futures([future], timeout=10)
 
         mock_plugin_1.process.assert_called_once_with(doc)
         mock_plugin_2.process.assert_called_once_with(doc)
         mock_plugin_3.process.assert_called_once_with(doc)
 
-    @skip('TODO')  # TODO
-    def test_error_handling(self):
-        pass
+    def test_handle_error_handling(self):
+        # Given
+        mocked_plugin_1 = Mock()
+        mocked_plugin_1.__class__.__name__ = 'Boum!'
+        mocked_plugin_1.process.side_effect = Exception(mocked_plugin_1.__class__.__name__)
+        mocked_plugin_2 = Mock()
+        mocked_plugin_2.__class__.__name__ = 'OK'
+        mocked_plugin_3 = Mock()
+        mocked_plugin_3.__class__.__name__ = 'Badaboum!'
+        mocked_plugin_3.process.side_effect = Exception(mocked_plugin_3.__class__.__name__)
+
+        self.processing.plugins = [mocked_plugin_1, mocked_plugin_2, mocked_plugin_3]
+        mocked_doc = Mock()
+
+        # When
+        logger_name = ftl_processing.logger.name
+        with self.assertLogs(logger_name, 'ERROR') as error_logs:
+            future = self.processing.apply_processing(mocked_doc)
+            wait_futures([future], timeout=10)
+
+        # Then
+        self.assertEqual(len(error_logs.output), 3)
+        self.assertIn(mocked_plugin_1.__class__.__name__, error_logs.output[0])
+        self.assertNotIn(mocked_plugin_2.__class__.__name__, error_logs.output[1])
+        self.assertIn(mocked_plugin_3.__class__.__name__, error_logs.output[1])
 
 
 class ProcLangTests(TestCase):
@@ -101,22 +128,48 @@ class ProcPGsqlTests(TestCase):
         doc.save.assert_called_once()
 
 
-class ProcAwsTextractTests(TestCase):
+class FTLOCRBaseTests(TestCase):
 
-    @skip('TODO')  # TODO
-    def test_process(self):
-        pass
+    @patch.object(FTLOCRBase, '_extract_text')
+    def test_process(self, mocked_extract_text):
+        expected_extracted_text = 'bingo!'
+        mocked_extract_text.return_value = expected_extracted_text
+        base_ocr = FTLOCRBase()
+        base_ocr.supported_storages.append(DEFAULT_FILE_STORAGE)
 
+        # When doc do not have a content text
+        mocked_doc = Mock()
+        mocked_doc.content_text = ''
 
-class ProcGoogleVisionTests(TestCase):
+        base_ocr.process(mocked_doc)
 
-    @skip('TODO')  # TODO
-    def test_process(self):
-        pass
+        # Then OCR _extract_text method is called
+        mocked_extract_text.assert_called_once()
+        self.assertEqual(mocked_doc.content_text, expected_extracted_text)
+        mocked_doc.save.assert_called_once()
 
+        # When doc already have a content text
+        mocked_extract_text.reset_mock()
+        original_doc_content = ':)'
+        mocked_doc = Mock()
+        mocked_doc.content_text = original_doc_content
 
-class ProcOcrMyPdfTests(TestCase):
+        base_ocr.process(mocked_doc)
 
-    @skip('TODO')  # TODO
-    def test_process(self):
-        pass
+        # Then OCR _extract_text method is skipped
+        mocked_extract_text.assert_not_called()
+        self.assertEqual(mocked_doc.content_text, original_doc_content)
+        mocked_doc.save.assert_not_called()
+
+    @patch.object(FTLOCRBase, '_extract_text')
+    def test_process_with_invalid_storage(self, mocked_extract_text):
+        base_ocr = FTLOCRBase()
+
+        # When DEFAULT_FILE_STORAGE does not match supported storages
+        base_ocr.supported_storages.append('this.is.not.the.default.storage')
+        mocked_doc = Mock()
+        mocked_doc.content_text = ''
+
+        # Then PluginUnsupportedStorage is raised
+        with self.assertRaises(PluginUnsupportedStorage):
+            base_ocr.process(mocked_doc)
