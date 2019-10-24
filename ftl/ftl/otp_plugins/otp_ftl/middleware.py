d31c7f234c18cffac62f5479a6cbd8214e3c7eae
Antoine L <antoine@exotic-matter.fr>
2019-10-24 16:46:59 +0200

    Fix: disable 2fa check in api when there is no active 2fa device for user

--- ftl/ftl/otp_plugins/otp_ftl/middleware.py
+++ ftl/ftl/otp_plugins/otp_ftl/middleware.py
@@ -1,3 +1,4 @@
+from django_otp import user_has_device
 from rest_framework.authentication import SessionAuthentication
 
 
@@ -5,7 +6,7 @@ class FTLSessionAuthentication(SessionAuthentication):
     def authenticate(self, request):
         response = super().authenticate(request)
 
-        if response and response[0] and response[0].is_verified():
+        if response and response[0] and (not user_has_device(response[0]) or response[0].is_verified()):
             return response
         else:
             return None
