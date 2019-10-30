4ad9096209ea81db5e9940c3d71279754870dc7f
fabien <fabien@exotic-matter.fr>
2019-10-30 16:16:37 +0100

    Use monkey patching to simplify deactivation of API rate limit in `settings_local` and `settings_tests`

--- ftl/core/urls.py
+++ ftl/core/urls.py
@@ -15,7 +15,6 @@ urlpatterns = [
     path('api/v1/documents/<uuid:pid>', views.FTLDocumentDetail.as_view()),
     path('api/v1/documents/<uuid:pid>/thumbnail.png', views.FTLDocumentThumbnail.as_view(), name='api_thumbnail_url'),
     path('api/v1/documents/upload', views.FileUploadView.as_view()),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     # path('api/token/verify', TokenVerifyView.as_view(), name='token_verify'),
