714daff74ac1c9a696fc56c872d2d6f117fa0c52
Antoine L <antoine@exotic-matter.fr>
2019-11-19 13:18:35 +0100

    Changing email now sends an email to validate.
    Using Django signing class and based on Django registration process.

--- ftl/account/forms.py
+++ ftl/account/forms.py
@@ -1,10 +1,5 @@
 from django import forms
-from django.contrib.auth import get_user_model
 
 
-class EmailUpdateForm(forms.ModelForm):
+class EmailSendForm(forms.Form):
     email = forms.CharField(widget=forms.EmailInput)
-
-    class Meta:
-        model = get_user_model()
-        fields = ['email']
--- ftl/account/views.py
+++ ftl/account/views.py
@@ -1,16 +1,23 @@
+from datetime import timedelta
+
+from django.contrib import messages
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.sites.shortcuts import get_current_site
+from django.core import signing
+from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.template.loader import render_to_string
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import FormView, UpdateView
+from django.views.generic import FormView
 
-from account.forms import EmailUpdateForm
+from account.forms import EmailSendForm
 
 
 class AccountView(LoginRequiredMixin, View):
-
     def get(self, request, *args, **kwargs):
         context = {
             'ftl_account': {'name': request.user.get_username(),  # get_username now return email
@@ -19,11 +26,13 @@ class AccountView(LoginRequiredMixin, View):
         return render(request, 'account/account_index.html', context)
 
 
-class AccountEmailView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+class AccountEmailChangeView(LoginRequiredMixin, SuccessMessageMixin, FormView):
     template_name = "account/account_email.html"
-    form_class = EmailUpdateForm
+    email_change_subject = "account/account_email_change_subject.txt"
+    email_change_body = "account/account_email_change_body.txt"
+    form_class = EmailSendForm
     success_url = reverse_lazy('account_index')
-    success_message = "Email successfully updated "
+    success_message = "A confirmation email has been sent to your new email."
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -31,8 +40,59 @@ class AccountEmailView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
                                   'isSuperUser': self.request.user.is_superuser}
         return context
 
-    def get_object(self, queryset=None):
-        return self.request.user
+    def get_email_context(self, activation_key):
+        scheme = 'https' if self.request.is_secure() else 'http'
+        return {
+            'scheme': scheme,
+            'activation_key': activation_key,
+            'expiration_minutes': 10,
+            'site': get_current_site(self.request)
+        }
+
+    def form_valid(self, form):
+        email_ = form.cleaned_data['email']
+
+        activation_key = signing.dumps(
+            obj=email_,
+            salt='email_change'
+        )
+
+        subject = render_to_string(
+            template_name=self.email_change_subject,
+            context=self.get_email_context(activation_key),
+            request=self.request
+        )
+
+        subject = ''.join(subject.splitlines())
+        message = render_to_string(
+            template_name=self.email_change_body,
+            context=self.get_email_context(activation_key),
+            request=self.request
+        )
+
+        send_mail(subject, message, None, [email_])
+
+        return super().form_valid(form)
+
+
+class AccountEmailChangeValidateView(LoginRequiredMixin, View):
+    success_message = "Email successfully updated"
+
+    def get(self, request, *args, **kwargs):
+        if 'token' in kwargs:
+            email = signing.loads(
+                kwargs['token'],
+                salt='email_change',
+                max_age=timedelta(minutes=10)
+            )
+
+            user = self.request.user
+            user.email = email
+            user.save()
+
+            messages.success(self.request, self.success_message)
+
+        return HttpResponseRedirect(reverse_lazy('account_index'))
 
 
 class AccountPasswordView(LoginRequiredMixin, FormView):
