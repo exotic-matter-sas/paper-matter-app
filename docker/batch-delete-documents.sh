4ea67ab4411e73c8efda6ce5821ae78faaaf86d3
Antoine L <antoine@exotic-matter.fr>
2020-01-16 11:59:18 +0100

    Use CRON_SECRET_KEY env var in local deployment

--- docker/batch-delete-documents.sh
+++ docker/batch-delete-documents.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 if [ -z "$CRON_DISABLE" ]; then
-  curl -H "X-Appengine-Cron: true" http://localhost:$PORT/crons/not-secure/batch-delete-documents
+  curl -H "X-Appengine-Cron: true" http://localhost:$PORT/crons/$CRON_SECRET_KEY/batch-delete-documents
   touch /tmp/batch-delete-docs-last-run
 fi
 
