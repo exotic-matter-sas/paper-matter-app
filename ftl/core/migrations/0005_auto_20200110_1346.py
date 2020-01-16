d0043baa53f79205aa64e6ad9ccf0f3a7344a107
Antoine L <antoine@exotic-matter.fr>
2020-01-16 17:11:31 +0100

    Due du merge, update migration script order

--- ftl/core/migrations/0005_auto_20200110_1346.py
+++ ftl/core/migrations/0005_auto_20200110_1346.py
@@ -9,7 +9,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0003_auto_20191207_1626'),
+        ('core', '0004_ftldocument_deleted'),
     ]
 
     operations = [
