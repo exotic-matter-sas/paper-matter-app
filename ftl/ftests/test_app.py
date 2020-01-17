998b954e91e452f97d9b49549c1ca0f9d16aef78
fabien <fabien@exotic-matter.fr>
2020-01-17 21:33:09 +0100

    Try to fix `test_search_not_found_from_a_folder` on CI

--- ftl/ftests/test_app.py
+++ ftl/ftests/test_app.py
@@ -465,6 +465,7 @@ class SearchTests(LoginPage, HomePage, DocumentViewerModal):
 
         # User search something that isn't present in his document
         self.search_documents('this text doesn\'t exist')
+        time.sleep(1)  # Force additional wait to make test pass on CI
 
         with self.assertRaises(NoSuchElementException, msg='No document should be found by this search query'):
             self.get_elems(self.documents_thumbnails)
@@ -484,6 +485,7 @@ class SearchTests(LoginPage, HomePage, DocumentViewerModal):
         self.get_elem(self.folders_list_buttons).click()
         self.wait_folder_list_loaded()
         self.search_documents('this text doesn\'t exist')
+        time.sleep(1)  # Force additional wait to make test pass on CI
 
         with self.assertRaises(NoSuchElementException, msg='No document should be found by this search query'):
             self.get_elems(self.documents_thumbnails)
