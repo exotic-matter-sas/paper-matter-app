9c2e57d512a855bb5fcbd22441321bb7f550281b
fabien <fabien@exotic-matter.fr>
2020-01-30 18:17:07 +0100

    Simplify `add_attr` custom filter logic by using Json for its arg

--- ftl/ftl/otp_plugins/otp_ftl/templatetags/add_attr.py
+++ ftl/ftl/otp_plugins/otp_ftl/templatetags/add_attr.py
@@ -1,5 +1,6 @@
 #  Copyright (c) 2020 Exotic Matter SAS. All rights reserved.
 #  Licensed under the BSL License. See LICENSE in the project root for license information.
+import json
 
 from django import template
 
@@ -7,15 +8,7 @@ register = template.Library()
 
 
 @register.filter(name='add_attr')
-def add_attr(field, css):
-    attrs = {}
-    definition = css.split(',')
-
-    for d in definition:
-        if ':' not in d:
-            attrs['class'] = d
-        else:
-            key, val = d.split(':')
-            attrs[key] = val
+def add_attr(field, key_val_json):
+    attrs = json.loads(key_val_json)
 
     return field.as_widget(attrs=attrs)
