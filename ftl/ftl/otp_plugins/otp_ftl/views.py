93edab52767e70b1c41de7545d88879e0acae3b2
Antoine L <antoine@exotic-matter.fr>
2020-01-31 11:46:28 +0100

    "Try another way" for 2fa check section is now dynamic and reflects current user 2fa devices

--- ftl/ftl/otp_plugins/otp_ftl/views.py
+++ ftl/ftl/otp_plugins/otp_ftl/views.py
@@ -59,6 +59,13 @@ class OTPCheckView(View):
 
 
 class FTLBaseCheckView(LoginView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['have_fido2'] = Fido2Device.objects.filter(user=self.request.user, confirmed=True).exists()
+        context['have_totp'] = TOTPDevice.objects.filter(user=self.request.user, confirmed=True).exists()
+        context['have_static'] = StaticDevice.objects.filter(user=self.request.user, confirmed=True).exists()
+        return context
+
     def get_success_url(self):
         url = self.request.session.get('next', None)
         if url:
