e7c266d231e5af87b11d6e68f4a174b32af55581
Antoine L <antoine@exotic-matter.fr>
2020-02-14 15:20:52 +0100

    Added comment

--- ftl/ftl/otp_plugins/otp_ftl/middleware.py
+++ ftl/ftl/otp_plugins/otp_ftl/middleware.py
@@ -6,9 +6,14 @@ from rest_framework.authentication import SessionAuthentication
 
 
 class FTLSessionAuthentication(SessionAuthentication):
+    """
+    Implement authentication 2fa check for internal API calls in the frontend.
+    """
+
     def authenticate(self, request):
         response = super().authenticate(request)
 
+        # response[0] is the user
         if response and response[0] and (not user_has_device(response[0]) or response[0].is_verified()):
             return response
         else:
