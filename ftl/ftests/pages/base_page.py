d623ea4846aeb12babf4cbb575cfdc9b0eb1fd7e
Antoine L <antoine@exotic-matter.fr>
2020-03-16 12:20:52 +0100

    Maybe fix i18n test?

--- ftl/ftests/pages/base_page.py
+++ ftl/ftests/pages/base_page.py
@@ -88,6 +88,7 @@ class BasePage(LIVE_SERVER):
             mime_type_list = 'application/octet-stream'
             profile.set_preference('browser.helperApps.neverAsk.openFile', mime_type_list)
             profile.set_preference('browser.helperApps.neverAsk.saveToDisk', mime_type_list)
+            profile.update_preferences()
 
             options = FirefoxOptions()
             if TEST_BROWSER_HEADLESS:
