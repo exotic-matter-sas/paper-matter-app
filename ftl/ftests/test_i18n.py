ec90cff77f8ddc4bd5b46e6d97eb3e63de930af9
Antoine L <antoine@exotic-matter.fr>
2020-03-16 11:51:51 +0100

    Maybe fix i18n test?

--- ftl/ftests/test_i18n.py
+++ ftl/ftests/test_i18n.py
@@ -12,7 +12,7 @@ from ftl.settings import DEV_MODE
 
 
 class I18nTests(SetupPages, LoginPage, HomePage):
-    def setUp(self, browser=None, browser_locale='fr'):
+    def setUp(self, browser=None, browser_locale='fr-fr, fr'):
         super().setUp(browser_locale=browser_locale)
 
     def test_i18n_are_working(self):
