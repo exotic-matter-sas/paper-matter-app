2a3d89b975030fffb1f1971a86de828bbd4eec83
Antoine L <antoine@exotic-matter.fr>
2020-04-01 17:38:54 +0200

    We don't need to import the custom variables here. It's already been imported in vue.config.js

--- ftl/frontend/src/main.js
+++ ftl/frontend/src/main.js
@@ -40,8 +40,6 @@ import Vuex from 'vuex';
 import storeConfig from "./store/storeConfig";
 import i18n from './i18n';
 
-import './styles/_variables.scss';
-
 Vue.config.productionTip = false;
 
 // Font Awesome icons definition
