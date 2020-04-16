126c68bae16f77947dadaa13cfba39cbd6a62b77
Antoine L <antoine@exotic-matter.fr>
2020-04-16 12:20:08 +0200

    Missing otp middleware in settings_local for Dockerfile.

--- docker/settings_local.py
+++ docker/settings_local.py
@@ -21,6 +21,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django_otp.middleware.OTPMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "ftl.ftl_setup_middleware.FTLSetupMiddleware",
