97cbc7a25b633b863da6621ead4e8e152bc025cb
Antoine L <antoine@exotic-matter.fr>
2020-07-15 16:18:05 +0200

    Update API document download url

--- ftl/core/urls.py
+++ ftl/core/urls.py
@@ -7,12 +7,6 @@ from . import views
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
-    path("uploads/<str:uuid>", views.DownloadView.as_view(), name="api_download_url"),
-    path(
-        "uploads/<str:uuid>/<str:name>",
-        views.ViewDocument.as_view(),
-        name="document_view",
-    ),
     path("api/v1/folders", views.FTLFolderList.as_view()),
     path("api/v1/folders/<int:id>", views.FTLFolderDetail.as_view()),
     path("api/v1/documents", views.FTLDocumentList.as_view()),
@@ -23,6 +17,16 @@ urlpatterns = [
         name="api_thumbnail_url",
     ),
     path("api/v1/documents/upload", views.FileUploadView.as_view()),
+    path(
+        "api/v1/documents/<str:uuid>/download",
+        views.DownloadView.as_view(),
+        name="api_download_url",
+    ),
+    path(
+        "api/v1/documents/<str:uuid>/download/<str:name>",
+        views.ViewDocument.as_view(),
+        name="api_document_view",
+    ),
     path("api/token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
 ]
