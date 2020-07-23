a58c1925fcac9a1da8aa40c6a4be8b605b9b79dd
Antoine L <antoine@exotic-matter.fr>
2020-07-23 14:05:58 +0200

    Use FTL login page instead of default admin login page.

--- ftl/ftl/admin.py
+++ ftl/ftl/admin.py
@@ -1,5 +1,7 @@
 #  Copyright (c) 2020 Exotic Matter SAS. All rights reserved.
 #  Licensed under the BSL License. See LICENSE in the project root for license information.
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django_otp import user_has_device
 from django_otp.admin import OTPAdminSite
 
@@ -15,3 +17,13 @@ class FTLAdminSite(OTPAdminSite):
         else:
             # Call original django admin method
             return super(OTPAdminSite, self).has_permission(request)
+
+    def login(self, request, extra_context=None):
+        # redirect to ftl login instead of using the admin login page (for 2fa support)
+        resp = super().login(request, extra_context)
+
+        if not isinstance(resp, HttpResponseRedirect):
+            url = reverse("login", current_app=self.name)
+            resp = HttpResponseRedirect(url)
+
+        return resp
