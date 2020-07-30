5134ebf6a0f1521fb30bba49f5633b6ea9ec9528
Antoine L <antoine@exotic-matter.fr>
2020-07-30 16:05:26 +0200

    Fix `test_not_blocking_other_app`

--- ftl/ftl/test_admin_ip_filter_middleware.py
+++ ftl/ftl/test_admin_ip_filter_middleware.py
@@ -9,9 +9,6 @@ from django.urls import reverse_lazy
 @override_settings(FTL_ADMIN_ENABLE_IP_PROTECTION=True)
 @override_settings(FTL_ADMIN_IP_RANGE_ALLOWED=["127.0.0.1/32", "10.0.0.0/8"])
 class TestFTLAdminIPFilter(TestCase):
-    def setUp(self):
-        self.get_response_mock = Mock()
-
     def test_is_not_blocked_1(self):
         response = self.client.get(reverse_lazy("admin:login"), REMOTE_ADDR="127.0.0.1")
         self.assertEqual(response.status_code, 200)
@@ -28,7 +25,7 @@ class TestFTLAdminIPFilter(TestCase):
 
     def test_not_blocking_other_app(self):
         response = self.client.get(
-            reverse_lazy("home"), follow=True, REMOTE_ADDR="127.0.0.1"
+            reverse_lazy("home"), follow=True, REMOTE_ADDR="128.0.0.1"
         )
         self.assertEqual(response.status_code, 200)
 
