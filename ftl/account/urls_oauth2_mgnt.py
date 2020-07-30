e27950698f7ba93fe7409eef60d4106239f36a86
Antoine L <antoine@exotic-matter.fr>
2020-07-30 15:50:33 +0200

    Require 2FA to enter OAuth2 management.

--- ftl/account/urls_oauth2_mgnt.py
+++ ftl/account/urls_oauth2_mgnt.py
@@ -1,12 +1,24 @@
 #  Copyright (c) 2020 Exotic Matter SAS. All rights reserved.
 #  Licensed under the BSL License. See LICENSE in the project root for license information.
 from django.conf.urls import url
+from django_otp.decorators import otp_required
 from oauth2_provider import views
 
 app_name = "oauth2_provider"
 
 urlpatterns = [
-    # Application management views
+    # Token management views
+    url(
+        r"^authorized_tokens/$",
+        otp_required(views.AuthorizedTokensListView.as_view(), if_configured=True),
+        name="authorized-token-list",
+    ),
+    url(
+        r"^authorized_tokens/(?P<pk>[\w-]+)/delete/$",
+        otp_required(views.AuthorizedTokenDeleteView.as_view(), if_configured=True),
+        name="authorized-token-delete",
+    ),
+    # API Application management views
     # url(r"^applications/$", views.ApplicationList.as_view(), name="list"),
     # url(
     #     r"^applications/register/$",
@@ -28,15 +40,4 @@ urlpatterns = [
     #     views.ApplicationUpdate.as_view(),
     #     name="update",
     # ),
-    # Token management views
-    url(
-        r"^authorized_tokens/$",
-        views.AuthorizedTokensListView.as_view(),
-        name="authorized-token-list",
-    ),
-    url(
-        r"^authorized_tokens/(?P<pk>[\w-]+)/delete/$",
-        views.AuthorizedTokenDeleteView.as_view(),
-        name="authorized-token-delete",
-    ),
 ]
