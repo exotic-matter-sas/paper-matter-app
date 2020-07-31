8bbed97cf720f6924d1d16b7d409a42d842f154a
Antoine L <antoine@exotic-matter.fr>
2020-07-31 15:57:21 +0200

    Fix IP filter tests due to change of admin form to ftl app login form (follow redirection).

--- ftl/ftl/test_admin_ip_filter_middleware.py
+++ ftl/ftl/test_admin_ip_filter_middleware.py
@@ -9,12 +9,14 @@ from django.urls import reverse_lazy
 @override_settings(FTL_ADMIN_IP_RANGE_ALLOWED=["127.0.0.1/32", "10.0.0.0/8"])
 class TestFTLAdminIPFilter(TestCase):
     def test_is_not_blocked_1(self):
-        response = self.client.get(reverse_lazy("admin:login"), REMOTE_ADDR="127.0.0.1")
+        response = self.client.get(
+            reverse_lazy("admin:login"), follow=True, REMOTE_ADDR="127.0.0.1"
+        )
         self.assertEqual(response.status_code, 200)
 
     def test_is_not_blocked_2(self):
         response = self.client.get(
-            reverse_lazy("admin:login"), REMOTE_ADDR="10.42.0.10"
+            reverse_lazy("admin:login"), follow=True, REMOTE_ADDR="10.42.0.10"
         )
         self.assertEqual(response.status_code, 200)
 
@@ -30,7 +32,9 @@ class TestFTLAdminIPFilter(TestCase):
 
     @override_settings(FTL_ADMIN_ENABLE_IP_PROTECTION=False)
     def test_not_blocking_when_disabled(self):
-        response = self.client.get(reverse_lazy("admin:login"), REMOTE_ADDR="128.0.0.1")
+        response = self.client.get(
+            reverse_lazy("admin:login"), follow=True, REMOTE_ADDR="128.0.0.1"
+        )
         self.assertEqual(response.status_code, 200)
 
 
@@ -43,7 +47,9 @@ class TestFTLAdminIPFilter(TestCase):
 class TestFTLAdminIPFilterForwardHeader(TestCase):
     def test_ip_header_forwarding_with_lb_not_blocked(self):
         response = self.client.get(
-            reverse_lazy("admin:login"), HTTP_X_FORWARDED_FOR="127.0.0.1, 190.0.0.1"
+            reverse_lazy("admin:login"),
+            follow=True,
+            HTTP_X_FORWARDED_FOR="127.0.0.1, 190.0.0.1",
         )
         self.assertEqual(response.status_code, 200)
 
@@ -65,6 +71,7 @@ class TestFTLAdminIPFilterProxyCount(TestCase):
         # Left most IP should be picked
         response = self.client.get(
             reverse_lazy("admin:login"),
+            follow=True,
             HTTP_X_FORWARDED_FOR="127.0.0.1, 190.0.0.1, 145.0.0.3, 94.45.67.240",
         )
         self.assertEqual(response.status_code, 200)
@@ -72,7 +79,9 @@ class TestFTLAdminIPFilterProxyCount(TestCase):
     @override_settings(FTL_ADMIN_IP_PROXY_COUNT=1)
     def test_ip_proxy_count_1(self):
         response = self.client.get(
-            reverse_lazy("admin:login"), HTTP_X_FORWARDED_FOR="127.0.0.1, 190.0.0.1",
+            reverse_lazy("admin:login"),
+            follow=True,
+            HTTP_X_FORWARDED_FOR="127.0.0.1, 190.0.0.1",
         )
         self.assertEqual(response.status_code, 200)
 
@@ -80,6 +89,7 @@ class TestFTLAdminIPFilterProxyCount(TestCase):
     def test_ip_proxy_count_2(self):
         response = self.client.get(
             reverse_lazy("admin:login"),
+            follow=True,
             HTTP_X_FORWARDED_FOR="127.0.0.1, 190.0.0.1, 145.0.0.3",
         )
         self.assertEqual(response.status_code, 200)
