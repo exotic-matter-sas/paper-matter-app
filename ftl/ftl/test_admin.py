3bf80244dbd103b9aee217043240353de40c6aa9
fabien <fabien@exotic-matter.fr>
2020-08-04 09:45:53 +0200

    Refactor tests names [skip ci]

--- ftl/ftl/test_admin.py
+++ ftl/ftl/test_admin.py
@@ -12,23 +12,23 @@ from ftests.tools.setup_helpers import (
 )
 
 
-class AdminFTLTests(TestCase):
+class AdminLoginFTLTests(TestCase):
     def setUp(self):
         self.org = setup_org()
         self.admin = setup_admin(org=self.org)
         self.org_1 = setup_org(name=tv.ORG_NAME_2, slug=tv.ORG_SLUG_2)
         self.user_1 = setup_user(self.org_1)
 
-    def test_admin_already_logged(self):
+    def test_admin_user_already_logged(self):
         setup_authenticated_session(self.client, self.org, self.admin)
         response = self.client.get(reverse("admin:index"), follow=True)
         self.assertContains(response, "Welcome")
 
-    def test_admin_login_failed(self):
+    def test_non_admin_user_already_logged(self):
         setup_authenticated_session(self.client, self.org_1, self.user_1)
         response = self.client.get(reverse("admin:login"), follow=True)
         self.assertEqual(response.status_code, 403)
 
-    def test_admin_login_not_logged_redirect(self):
+    def test_not_logged(self):
         response = self.client.get(reverse("admin:index"), follow=True)
         self.assertRedirects(response, f"{reverse('login')}?next=/admin/")
