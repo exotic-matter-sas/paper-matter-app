22ac0745c01a980defe357f1d8b1062c3b055f6c
fabien <fabien@exotic-matter.fr>
2021-02-03 23:09:00 +0100

    Fix `test_rename_auth_app` on Chrome

--- ftl/ftests/pages/account_pages.py
+++ ftl/ftests/pages/account_pages.py
@@ -131,6 +131,7 @@ class AccountPages(BasePage):
     def rename_auth_app(self, new_name, set_index=0):
         self.get_elems(self.rename_auth_app_buttons)[set_index].click()
         self.wait_for_elem_to_disappear(self.rename_auth_app_buttons)
+        self.get_elem(self.device_name_input).click()
         self.get_elem(self.device_name_input).send_keys(new_name)
         self.get_elem(self.confirm_button).click()
         self.wait_for_elem_to_disappear(self.device_name_input)
