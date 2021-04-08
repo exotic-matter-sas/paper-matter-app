4cfbe6bf88ea3dca2edc782fbec0a26dcc6fa0c0
Antoine L <antoine@exotic-matter.fr>
2021-04-08 21:32:25 +0200

    Upgrade Django to 2.2.20
    Fix `test_user_have_region_settings`

--- ftl/ftests/test_account.py
+++ ftl/ftests/test_account.py
@@ -3,8 +3,10 @@
 
 import re
 import threading
+from datetime import datetime
 from unittest.mock import patch, Mock
 
+import pytz
 from django.core import mail
 from django.http import HttpResponse
 from django.test import override_settings
@@ -222,7 +224,9 @@ class RegionAccountPageTests(LoginPage, HomePage, AccountPages, SignupPages):
         # Locale come from user setting
         self.assertIn("account", self.head_title)
         # Timezone come from user setting
-        self.assertEqual("CET", self.get_elem_attribute("#current-time", "title"))
+        tz = pytz.timezone(user_timezone_setting)
+        tz_str = tz.localize(datetime.utcnow()).strftime("%Z")
+        self.assertEqual(tz_str, self.get_elem_attribute("#current-time", "title"))
 
     def test_user_update_region_language(self):
         setup_user(self.user_org)
